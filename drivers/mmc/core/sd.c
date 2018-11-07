9746c21ecb6ba84f4de9c92eb143a35e5af839d9
c457 <android.c357@gmail.com>
2018-11-08 00:55:11 +0100

    mmc: core: Import ZTE changes

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1161,12 +1161,9 @@ static void mmc_sd_detect(struct mmc_host *host)
 	 * just return; This is to ensure that when this call is invoked
 	 * due to pm_suspend, not to block suspend for longer duration.
 	 */
-	pm_runtime_get_sync(&host->card->dev);
-	if (!mmc_try_claim_host(host, 2000)) {
-		pm_runtime_mark_last_busy(&host->card->dev);
-		pm_runtime_put_autosuspend(&host->card->dev);
+	if (mmc_try_get_card(host->card, 3000))
 		return;
-	}
+
 #ifdef CONFIG_MMC_BLOCK_DEFERRED_RESUME
 	if (mmc_bus_needs_resume(host))
 		mmc_resume_bus(host);
@@ -1480,6 +1477,7 @@ int mmc_attach_sd(struct mmc_host *host)
 	if (!retries) {
 		printk(KERN_ERR "%s: mmc_sd_init_card() failure (err = %d)\n",
 		       mmc_hostname(host), err);
+		host->is_bad_card = true;
 		goto err;
 	}
 #else
