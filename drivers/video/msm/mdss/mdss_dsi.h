e723dcfd42d0ddbd625a1657f10ede4457132fe7
c457 <android.c357@gmail.com>
2018-11-08 01:06:56 +0100

    msm: mdss: Import ZTE changes

--- drivers/video/msm/mdss/mdss_dsi.h
+++ drivers/video/msm/mdss/mdss_dsi.h
@@ -24,6 +24,12 @@
 #include "mdss_dsi_cmd.h"
 #include "mdss_dsi_clk.h"
 
+/*zte add common function for lcd module begin*/
+#ifdef CONFIG_ZTE_LCD_COMMON_FUNCTION
+#include "zte_lcd_common.h"
+#endif
+/*zte add common function for lcd module end*/
+
 #define MMSS_SERDES_BASE_PHY 0x04f01000 /* mmss (De)Serializer CFG */
 
 #define MIPI_OUTP(addr, data) writel_relaxed((data), (addr))
@@ -432,9 +438,18 @@ struct mdss_dsi_ctrl_pdata {
 	int rst_gpio;
 	int disp_en_gpio;
 	int bklt_en_gpio;
+	/*zte,esd interrupt mode 0205  start */
+	int lcd_esd_interrupt_gpio;
+	int lcd_esd_panel_error_flag;
+	/*zte,esd interrupt mode 0205  end */
+	int lcd_3v_vsp_en_gpio;
 	int mode_gpio;
 	int intf_mux_gpio;
 	int bklt_ctrl;	/* backlight ctrl */
+	/*zte,for acl and hbm  20161224 start */
+	int current_acl_level;
+	int current_hbm_level;
+	/*zte,for acl and hbm  20161224 end */
 	bool pwm_pmi;
 	int pwm_period;
 	int pwm_pmic_gpio;
@@ -512,6 +527,7 @@ struct mdss_dsi_ctrl_pdata {
 	char dlane_swap;	/* data lane swap */
 	bool is_phyreg_enabled;
 	bool burst_mode_enabled;
+	bool is_cmdlist_from_mdp;
 
 	struct dsi_buf tx_buf;
 	struct dsi_buf rx_buf;
@@ -557,6 +573,12 @@ struct mdss_dsi_ctrl_pdata {
 	bool update_phy_timing; /* flag to recalculate PHY timings */
 
 	bool phy_power_off;
+
+/*zte add common function for lcd module begin*/
+#ifdef CONFIG_ZTE_LCD_COMMON_FUNCTION
+	struct zte_lcd_ctrl_data zte_lcd_ctrl;
+#endif
+/*zte add common function for lcd module end*/
 };
 
 struct dsi_status_data {
@@ -596,6 +618,9 @@ int mdss_dsi_wait_for_lane_idle(struct mdss_dsi_ctrl_pdata *ctrl);
 irqreturn_t mdss_dsi_isr(int irq, void *ptr);
 irqreturn_t hw_vsync_handler(int irq, void *data);
 void disable_esd_thread(void);
+/*zte,esd interrupt mode 0205  start */
+irqreturn_t esd_gpio_interrupt_handler(int irq, void *data);
+/*zte,esd interrupt mode 0205  end */
 void mdss_dsi_irq_handler_config(struct mdss_dsi_ctrl_pdata *ctrl_pdata);
 
 void mdss_dsi_set_tx_power_mode(int mode, struct mdss_panel_data *pdata);
@@ -626,6 +651,7 @@ int mdss_dsi_post_clkon_cb(void *priv,
 int mdss_dsi_pre_clkon_cb(void *priv,
 			  enum mdss_dsi_clk_type clk_type,
 			  enum mdss_dsi_clk_state new_state);
+void mdss_dsi_panel_3v_power(struct mdss_panel_data *pdata, int enable);
 int mdss_dsi_panel_reset(struct mdss_panel_data *pdata, int enable);
 void mdss_dsi_phy_disable(struct mdss_dsi_ctrl_pdata *ctrl);
 void mdss_dsi_cmd_test_pattern(struct mdss_dsi_ctrl_pdata *ctrl);
@@ -846,6 +872,14 @@ static inline bool mdss_dsi_is_te_based_esd(struct mdss_dsi_ctrl_pdata *ctrl)
 		gpio_is_valid(ctrl->disp_te_gpio) &&
 		mdss_dsi_is_left_ctrl(ctrl);
 }
+
+/*zte,esd interrupt mode 0205  start */
+static inline bool mdss_dsi_is_gpio_interrupt_esd(struct mdss_dsi_ctrl_pdata *ctrl)
+{
+	return gpio_is_valid(ctrl->lcd_esd_interrupt_gpio) &&
+		mdss_dsi_is_left_ctrl(ctrl);
+}
+/*zte,esd interrupt mode 0205 end */
 
 static inline struct mdss_dsi_ctrl_pdata *mdss_dsi_get_ctrl_clk_master(void)
 {
